31d40fc553cc553b86810c03fe59ccd3196eec48
AuxZarske <43278023+AuxZarske@users.noreply.github.com>
2024-02-12 19:27:53 -0300

    se agrega test de creacion para la tarea, verificando que la solicitud de una respuesta correcta y que la tarea se guarde en la BD

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,3 +1,32 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework_simplejwt.tokens import AccessToken
+from .models import Task
 
-# Create your tests here.
+
+class TaskCreationTestCase(APITestCase):
+
+    def setUp(self):
+        User = get_user_model()
+        self.user = User.objects.create_user(email="test_normal@user.com", password="foo")
+        
+
+    def test_task_creation(self):
+    
+        token = AccessToken.for_user(self.user)
+        
+        task_data = {
+            'title': 'Tarea de prueba',
+            'description': 'Esta es una tarea de prueba' 
+        }
+
+        headers = {'Authorization': f'Bearer {token}'}
+
+        response = self.client.post(reverse('task-list'), task_data,headers=headers)
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        self.assertTrue(Task.objects.filter(title='Tarea de prueba').exists())
+
