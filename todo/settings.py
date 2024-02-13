38d7724e4697b9db8637476b7b79e36f31db1d37
AuxZarske <43278023+AuxZarske@users.noreply.github.com>
2024-02-13 15:17:43 -0300

    se agrega el documento como ejemplo de variables de entorno a utilizar

--- todo/settings.py
+++ todo/settings.py
@@ -7,13 +7,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(xtefpatqlm#hiuvvkpwmi6kkq9x5_i^^vjbf(p(-bm8b^z)f8'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = config(
+    'ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')]
+)
 
 # Application definition
 
